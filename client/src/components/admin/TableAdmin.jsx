f40de8144a05677ca4f2e698a81761ddc49d77bb
Lucas-Hr <lucashenintsoarandriamiarisoa@gmail.com>
2025-05-09 21:46:22 +0300

    Added props rows to Table Component

--- client/src/components/admin/TableAdmin.jsx
+++ client/src/components/admin/TableAdmin.jsx
@@ -8,27 +8,17 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 
-const TableAdmin = ({header}) => {
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-      }
-      
-      const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-      ];
+const TableAdmin = ({header,rows}) => {
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            {header.map((row,index) => {
+            {header.map((h,index) => {
                 return (
                     <TableCell align='left' key={index}>
-                        {row}
+                        {h}
                     </TableCell>
                 )
             }
@@ -41,13 +31,21 @@ const TableAdmin = ({header}) => {
               key={index}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="left">{row.calories}</TableCell>
-              <TableCell align="left">{row.fat}</TableCell>
-              <TableCell align="left">{row.carbs}</TableCell>
-              <TableCell align="left">{row.protein}</TableCell>
+              {header.map((h,index) => (
+                <TableCell key={index} align='left'>
+                  {h === "Social Media"  ? (
+                  <ul>
+                    {Object.entries(row[h]).map(([platform, link]) => (
+                      <li key={platform}>
+                        <strong>{platform}:</strong> {link}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  row[h]
+                )}
+                </TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
